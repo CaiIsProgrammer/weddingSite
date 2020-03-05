316000b51263567cd365722528bdc32c17bdec78
Cai Martin <cmartin.professional@yahoo.com>
2020-03-05 17:34:04 -0500

    Updates

---
+++ precache-manifest.b0bfa89ac7b21d8281b984283b18f06c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6483f5017d7f34a63b98afd9bf585cc9",
+    "url": "/weddingSite/index.html"
+  },
+  {
+    "revision": "90d2114528b50a6333ef",
+    "url": "/weddingSite/static/css/main.54f1e18e.chunk.css"
+  },
+  {
+    "revision": "40df2fb6a38fd4ed5193",
+    "url": "/weddingSite/static/js/2.98a2e53f.chunk.js"
+  },
+  {
+    "revision": "90d2114528b50a6333ef",
+    "url": "/weddingSite/static/js/main.ba668dd5.chunk.js"
+  },
+  {
+    "revision": "55f64899fd262f9be931",
+    "url": "/weddingSite/static/js/runtime~main.d1b7907e.js"
+  }
+]);
