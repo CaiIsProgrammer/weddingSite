b5b42b06adcb9d2a6417f9aaac7d5667d57c9ed2
Cai Martin <cmartin.professional@yahoo.com>
2020-03-05 17:52:01 -0500

    Wedding Site

--- src/index.js
+++ src/index.js
@@ -1,11 +1,93 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import "./styles.css";
+import DateCountdown from "react-date-countdown-timer";
+import Particles from "react-particles-js";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Count down to wedding</h1>
+        <DateCountdown dateTo="June 1, 2020 00:00:00" />
 
-import App from "./App";
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%"
+          }}
+        >
+          <Particles
+            params={{
+              particles: {
+                number: {
+                  value: 500,
+                  density: {
+                    enable: false
+                  }
+                },
+                shape: {
+                  type: ["images", "circle"],
+                  images: [
+                    {
+                      src: "/heart.svg",
+                      height: 500,
+                      width: 500
+                    }
+                  ]
+                },
+                color: {
+                  value: "#CCC"
+                },
+                size: {
+                  value: 5,
+                  random: true,
+                  anim: {
+                    speed: 4,
+                    size_min: 1
+                  }
+                },
+                line_linked: {
+                  enable: false
+                },
+                move: {
+                  random: true,
+                  speed: 1,
+                  direction: "top",
+                  out_mode: "out"
+                }
+              },
+              interactivity: {
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: "bubble"
+                  },
+                  onclick: {
+                    enable: true,
+                    mode: "repulse"
+                  }
+                },
+                modes: {
+                  bubble: {
+                    distance: 250,
+                    duration: 2,
+                    size: 0,
+                    opacity: 0
+                  },
+                  repulse: {
+                    distance: 400,
+                    duration: 4
+                  }
+                }
+              }
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
