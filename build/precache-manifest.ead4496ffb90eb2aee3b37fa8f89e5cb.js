db17cd948305f6041f9f91f9fce6ec03872f7b59
Cai Martin <cmartin.professional@yahoo.com>
2020-03-05 19:18:25 -0500

    fixing for netlify

---
+++ build/precache-manifest.ead4496ffb90eb2aee3b37fa8f89e5cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9556c26472286b4bdc1eff590f2f16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "893e07c8e4f8290f3706",
+    "url": "/static/css/main.54f1e18e.chunk.css"
+  },
+  {
+    "revision": "186b6c4af71037cc3637",
+    "url": "/static/js/2.810e448b.chunk.js"
+  },
+  {
+    "revision": "893e07c8e4f8290f3706",
+    "url": "/static/js/main.842f1d5e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0984040eef9233c2763369c6eb47b7f4",
+    "url": "/static/media/heart.0984040e.svg"
+  }
+]);
